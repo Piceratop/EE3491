6756b724806b69e4d1caf9c798c3ebcff3e64f95
buinhatminh2k52005 <buinhatminh2k52005@gmail.com>
2025-02-20 01:08:29 +0000

    refactor code

---
+++ Week1/ex1_6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef struct {
+    double real;
+    double imaginary;
+} Complex;
+
+int main() {
+    double a, b, c;
+    printf("Enter the coefficients of the quadratic variables\n");
+    printf("(Enter a, b, c of ax^2+bx+c = 0, each seperated by a space.):\n");
+    scanf("%lf %lf %lf")
+}
